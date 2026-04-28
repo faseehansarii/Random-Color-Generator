bcabd3d1f8b47cc45e0644d4456ac231362cc21c
Faseeh Ansari <faseehislive@gmail.com>
2026-04-28 17:40:36 +0500

    release

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let prevClr=[];
 let box = document.querySelector("#box");
 let h1 = document.querySelector("h1");
 let btn = document.querySelector("button");
+let paletes=document.querySelectorAll(".palete");
 
 btn.addEventListener("click",function(){
 
@@ -22,10 +23,22 @@ btn.addEventListener("click",function(){
     
     //for previous colors
     array.push(color);
-    let palletes=document.querySelectorAll(".pallete");
-    let prevClr=array.slice(-5)                         //to get the last 5 colors
-    for(let i=0;i<=5;i++){
-        palletes[i].style.backgroundColor=prevClr[i];         
-    }
+    let prevClr=array.slice(-5);                        //to get the last 5 colors
+    for(let i=0;i<5;i++){
+        paletes[i].style.backgroundColor=prevClr[i];
 
+        // pop animation: remove class first, then re-add to retrigger it
+        paletes[i].classList.remove("pop");
+        void paletes[i].offsetWidth;                    
+        if(prevClr[i]) paletes[i].classList.add("pop");
+    }
 })
+
+//get any previous color's details
+for (palete of paletes){                                    
+        palete.addEventListener("click",function(e){
+           box.style.backgroundColor=e.target.style.backgroundColor;
+           h1.innerText=e.target.style.backgroundColor;
+           btn.style.borderColor=e.target.style.backgroundColor;
+        })
+    }
